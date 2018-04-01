f9e095605b29bdcffff37b2b8f28613809faf3aa
camilo <cyateya35439@universidadean.edu.co>
2018-04-01 00:27:37 -0500

    CREANDO LOS INPUTS

--- index.php
+++ index.php
@@ -134,13 +134,13 @@
 
 </table>
 <!--server.php conexion con la base de datos-->
-<form action="server.php" method="POST">
+<form action="server.php" class="ui form" method="POST">
     <!--//12) actualizar registros en el formulario-->
    <input type="hidden" name="empleado_id"  value="<?php echo $empleado_id;?>">
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
   <div class="ui right labeled input">
-        <input type="number" placeholder="Enter ..." autofocus maxlength="3" class="loading" name="empleado_id" value="<?php echo $empleado_id;?>">
+        <input type="number" placeholder="Enter ..." autofocus maxlength="3" name="empleado_id" value="<?php echo $empleado_id;?>">
         <div class="ui teal  label">
             empleado_id
         </div>
@@ -149,7 +149,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-    <div class="ui right labeled input loading">
+    <div class="ui right labeled input ">
         <input type="text" placeholder="Enter ..." autofocus maxlength="20" name="primer_nombre" value="<?php echo $primer_nombre;?>">
         <div class="ui teal   label">
             Primer Nombre
@@ -158,7 +158,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-   <div class="ui right labeled input loading">
+   <div class="ui right labeled input ">
         <input type="text" placeholder="Enter ..." autofocus maxlength="20" name="segundo_nombre" value="<?php echo $segundo_nombre;?>">
         <div class="ui teal   label">
             Segundo Nombre
@@ -166,7 +166,7 @@
     </div>
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-    <div class="ui right labeled input loading">
+    <div class="ui right labeled input ">
         <input type="email" placeholder="Enter ..." name="correo" autofocus maxlength="50" value="<?php echo $correo;?>">
         <div class="ui teal   label">
             Correo
@@ -175,7 +175,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-    <div class="ui right labeled input loading">
+    <div class="ui right labeled input ">
         <input type="number" placeholder="Enter ..." autofocus maxlength="15" name="numero_telefono" value="<?php echo $numero_telefono;?>">
         <div class="ui teal   label">
             Numero_Telefono
@@ -184,7 +184,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-   <div class="ui right labeled input loading">
+   <div class="ui right labeled input ">
         <input type="datetime-local" placeholder="Enter ..." name="fecha_ingreso" value="<?php echo $fecha_ingreso;?>">
         <div class="ui teal   label">
             Fecha Ingreso
@@ -193,7 +193,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-    <div class="ui right labeled input loading">
+    <div class="ui right labeled input ">
         <input type="text" placeholder="Enter ..." autofocus maxlength="20" name="trabajo_id" value="<?php echo $trabajo_id;?>">
         <div class="ui teal   label">
             Trabajo Id
@@ -202,7 +202,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-   <div class="ui right labeled input loading">
+   <div class="ui right labeled input ">
         <input type="number" placeholder="Enter ..." autofocus maxlength="14" name="salario" value="<?php echo $salario;?>">
         <div class="ui teal   label">
             Salario
@@ -211,7 +211,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-     <div class="ui right labeled input loading">
+     <div class="ui right labeled input ">
         <input type="number" placeholder="Enter ..." autofocus maxlength="3" name="jefe_id" value="<?php echo $jefe_id;?>">
         <div class="ui teal   label">
             Jefe Id
@@ -220,7 +220,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-    <div class="ui right labeled input loading">
+    <div class="ui right labeled input ">
         <input type="number" placeholder="Enter ..." autofocus maxlength="3" name="departamento_id" value="<?php echo $departamento_id;?>">
         <div class="ui teal   label">
             Departamento Id
@@ -229,7 +229,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-   <div class="ui right labeled input loading">
+   <div class="ui right labeled input ">
         <input type="text" placeholder="Enter ..." autofocus maxlength="1" name="sexo" value="<?php echo $sexo;?>">
         <div class="ui teal   label">
             Sexo
@@ -238,7 +238,7 @@
 
 
     <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
-    <div class="ui right labeled input loading">
+    <div class="ui right labeled input ">
         <input type="text" placeholder="Enter ..." autofocus maxlength="20" autofocus name="genero" value="<?php echo $genero;?>">
         <div class="ui teal   label">
             Genero
@@ -255,7 +255,7 @@
 
         </div>
     </div>-->
-    <div class="ui right labeled input loading">
+    <div class="ui right labeled input ">
     <select name="estado_civil" >
 
         <option value="<?php echo $estado_civil;?>">SOLTERO</option>
